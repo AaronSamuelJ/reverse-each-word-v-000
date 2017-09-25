ade3ef3f634c18f3aacd59db33bf90f1fa1b48c3
Aaron Samuel <aasamuelj@gmail.com>
2017-09-25 01:39:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,11 @@
+# def reverse_each_word(sentence)
+#   new_sentence = []
+#   arr = sentence.split(" ")
+#   arr.each do |word|
+#     new_sentence << word.reverse
+#   end
+#   new_sentence.join(" ")
+# end
 def reverse_each_word(sentence)
-  new_sentence = []
-  arr = sentence.split(" ")
-  arr.each do |word|
-    new_sentence << word.reverse
-  end
-  new_sentence.join(" ")
+  sentence.split(" ").collect{|word|word.reverse}.join(" ")
 end
